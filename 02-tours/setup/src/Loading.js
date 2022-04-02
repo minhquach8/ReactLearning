cba5f41949820ebdf4ed101973de8b87dc50c2ea
minhquach <minhquach8@gmail.com>
2022-04-02 10:21:16 +0700

    2/4/22 02 Done

--- 02-tours/setup/src/Loading.js
+++ 02-tours/setup/src/Loading.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
-  return (
-    <div className="loading">
-      <h1>loading...</h1>
-    </div>
-  );
+	return (
+		<div className="loading">
+			<h1>loading...</h1>
+		</div>
+	);
 };
 
 export default Loading;
